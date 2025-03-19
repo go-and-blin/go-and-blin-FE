b0ee3fa84215d8bfcff491f7f101a643b439eb8f
kang-sky <yallo428@gmail.com>
2025-03-19 20:51:03 +0900

    Refactor environment variable loading and config structure.

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,22 @@
 import {fileURLToPath, URL} from 'node:url'
 
-import {defineConfig, loadEnv} from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import path from 'path'
+import * as dotenv from "dotenv";
+import {resolve} from "node:path";
 
+dotenv.config({ path: resolve(__dirname, 'GAB-config/.env') })
 // https://vite.dev/config/
-export default defineConfig(() => {
-    const libEnv = loadEnv('', path.resolve(__dirname, 'GAB-config'));
+export default defineConfig({
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
 
-    return {
-        plugins: [
-            vue(),
-            vueDevTools(),
-        ],
-        define: {
-            'import.meta.env': {
-                ...libEnv
-            },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         },
-        resolve: {
-            alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url))
-            },
-        },
-    }
+    },
 })
